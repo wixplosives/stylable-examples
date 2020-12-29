c235bd7d57095935437c9f2c2c1e7dfc6fe97803
Barak Igal <baraki@wix.com>
2020-12-29 11:27:09 +0200

    prettify

--- packages/component-lib/webpack.config.js
+++ packages/component-lib/webpack.config.js
@@ -1,5 +1,6 @@
 const { join } = require('path');
 const { StylableWebpackPlugin } = require('@stylable/webpack-plugin');
+const { noCollisionNamespace } = require('@stylable/core');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
@@ -19,17 +20,17 @@ module.exports = {
                 loader: '@ts-tools/webpack-loader',
             },
             {
-                test: filePath => !filePath.endsWith('.st.css') && filePath.endsWith('.css'),
-                use: ['css-loader']
+                test: (filePath) => !filePath.endsWith('.st.css') && filePath.endsWith('.css'),
+                use: ['css-loader'],
             },
             {
                 test: /\.(png|jpg|gif|svg|woff2|ttf)$/i,
                 loader: 'url-loader',
                 options: {
-                    limit: 2048
-                }
-            }
-        ]
+                    limit: 2048,
+                },
+            },
+        ],
     },
     optimization: {
         splitChunks: {
@@ -40,28 +41,13 @@ module.exports = {
                 commons: {
                     test: /[\\/]node_modules[\\/]/,
                     name: 'vendors',
-                    chunks: 'all'
-                }
-            }
-        }
+                    chunks: 'all',
+                },
+            },
+        },
     },
     resolve: {
         extensions: ['.ts', '.tsx', '.mjs', '.js', '.json'],
     },
     plugins: [new HtmlWebpackPlugin(), new StylableWebpackPlugin({ resolveNamespace: noCollisionNamespace() })],
 };
-
-function noCollisionNamespace({ prefix = '', used: usedNamespaces = new Map() } = {}) {
-    return (namespace, stylesheetPath) => {
-        const ns = prefix + namespace;
-        const used = usedNamespaces.get(ns);
-        if (used) {
-            if (used.stylesheetPath !== stylesheetPath) {
-                throw new Error(`namespace (${ns} of ${stylesheetPath}) is already in use by ${used.stylesheetPath}`);
-            }
-        } else {
-            usedNamespaces.set(ns, { prefix, namespace, stylesheetPath });
-        }
-        return ns;
-    };
-}
--- packages/simple-app/webpack.config.js
+++ packages/simple-app/webpack.config.js
@@ -1,5 +1,6 @@
 const { join } = require('path');
 const { StylableWebpackPlugin } = require('@stylable/webpack-plugin');
+const { noCollisionNamespace } = require('@stylable/core');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
@@ -16,35 +17,20 @@ module.exports = {
                 loader: '@ts-tools/webpack-loader',
             },
             {
-                test: filePath => !filePath.endsWith('.st.css') && filePath.endsWith('.css'),
-                use: ['css-loader']
+                test: (filePath) => !filePath.endsWith('.st.css') && filePath.endsWith('.css'),
+                use: ['css-loader'],
             },
             {
                 test: /\.(png|jpg|gif|svg|woff2|ttf)$/i,
                 loader: 'url-loader',
                 options: {
-                    limit: 2048
-                }
-            }
-        ]
+                    limit: 2048,
+                },
+            },
+        ],
     },
     resolve: {
         extensions: ['.ts', '.tsx', '.mjs', '.js', '.json'],
     },
     plugins: [new HtmlWebpackPlugin(), new StylableWebpackPlugin({ resolveNamespace: noCollisionNamespace() })],
 };
-
-function noCollisionNamespace({ prefix = '', used: usedNamespaces = new Map() } = {}) {
-    return (namespace, stylesheetPath) => {
-        const ns = prefix + namespace;
-        const used = usedNamespaces.get(ns);
-        if (used) {
-            if (used.stylesheetPath !== stylesheetPath) {
-                throw new Error(`namespace (${ns} of ${stylesheetPath}) is already in use by ${used.stylesheetPath}`);
-            }
-        } else {
-            usedNamespaces.set(ns, { prefix, namespace, stylesheetPath });
-        }
-        return ns;
-    };
-}
