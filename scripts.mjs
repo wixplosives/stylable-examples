f510b3f5e215c46982b4d9dbe61a174c0b523aa9
barak igal <baraki@wix.com>
2022-11-15 14:53:41 +0200

    refactor: restructure repo (#769)

---
+++ scripts.mjs
@@ -0,0 +1,57 @@
+import { execSync } from 'child_process';
+
+const DEFAULT_PROJECTS_ORDER = ['component-library', 'simple-spa-app'];
+
+const [commandInput, ...projects] = process.argv.slice(2);
+const commands = commandInput?.split('+') ?? [];
+const projectsBuildOrder = projects.length ? projects : DEFAULT_PROJECTS_ORDER;
+
+if (!commands.length) {
+  console.log(redColor('No commands provided. Exiting.'));
+  process.exit(1);
+}
+
+if (!projectsBuildOrder.length) {
+  console.log(redColor('No projects provided. Exiting.'));
+  process.exit(1);
+}
+
+console.log(greenColor(`[Info] Running commands: ${commands.join(' -> ')}`));
+console.log(greenColor(`[Info] Projects build order: ${projectsBuildOrder.join(' -> ')} `));
+
+const operations = {
+  install: () => operateOnEach(projectsBuildOrder, install),
+  ci: () => operateOnEach(projectsBuildOrder, ci),
+  build: () => operateOnEach(projectsBuildOrder, build),
+};
+
+for (const command of commands) {
+  (await operations[command]?.()) ?? console.log(redColor(`[Error] Unknown command: ${command}`));
+}
+
+async function operateOnEach(projects, op) {
+  for (const project of projects) {
+    console.log(greenColor(`[Info] ${op.name} ${project} `));
+    await op(project);
+  }
+}
+
+function install(project) {
+  execSync(`npm install`, { cwd: project, stdio: 'inherit' });
+}
+
+function ci(project) {
+  execSync(`npm ci`, { cwd: project, stdio: 'inherit' });
+}
+
+function build(project) {
+  execSync(`npm run build`, { cwd: project, stdio: 'inherit' });
+}
+
+function greenColor(str) {
+  return `\x1b[32m${str}\x1b[0m`;
+}
+
+function redColor(str) {
+  return `\x1b[31m${str}\x1b[0m`;
+}
